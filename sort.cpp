8b098b61fb5330f164e9af75e1cae15d9c15e64c
Soham Banerjee <84960194+Soham0702@users.noreply.github.com>
2025-10-29 16:06:45 +0530

    Implement sorting of input data from file

---
+++ sort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>                                                                                                                                                                                                                                                                                              
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input-file>\n";
+        return 1;
+    }   
+
+    std::ifstream in(argv[1]);
+    std::ofstream out("details");
+
+    int nev =2500;
+
+
+    std::vector <int> iev(nev);
+    std::vector <double> en(nev);
+
+    std::string line;
+    int k=0;
+    while (std::getline(in,line))
+    {   
+        std::stringstream iss(line);
+        iss >>iev[k] >> en[k];
+        k++;
+    }   
+
+    auto sz = (iev.size() == en.size() &&  iev.size() == nev) ? nev : 0;
+
+    if(sz ==0 )
+    {   
+        std::cerr<<"Something wrong here !! "<<std::endl;
+        exit(1);
+    }   
+
+
+    for(int i=0;i<nev-1;i++)
+        for(int j =i+1;j<nev;j++)
+            if(en[j]>en[i])
+            {
+                std::swap( en[j],en[i]);
+                std::swap(iev[j],iev[i]);
+            }
+    
+
+    out<<std::fixed;
+    for(int i=0;i<sz;i++)
+        out<< iev[i]<<"\t"<< std::setprecision(10)<<en[i]<<std::endl;
+
+
+    return 0;
+}
