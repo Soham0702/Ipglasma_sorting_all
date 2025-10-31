e8c545d7b0e583a7473d1ab73c75e6aa7c1157f7
Soham Banerjee <84960194+Soham0702@users.noreply.github.com>
2025-10-31 17:04:42 +0530

    Update rename.sh to enhance file renaming process
    Refactor renaming logic and improve file handling.

--- rename.sh
+++ rename.sh
@@ -4,13 +4,33 @@ set -euo pipefail
 #i will rename all the files here and then move to a different folder for the fuck of it
 
 
-m=21
-c=1
-for ((i=1; i<m; i++))
+m=21                                                                                                                                                                                                                                                                                                             
+k=125
+for ((i=1; i<2; i++))
 do
         folder="run-$i"
         cd "$folder" || { echo "Failed to cd into $folder"; exit 1; }
-        
+    
+        for ((j=124;j>=0;j--))
+        do
+                echo "moving item $k"
+                mv "epsilon-u-Hydro-TauHydro-$j.dat" "epsilon-u-Hydro-TauHydro-$k.dat"
+                mv "NcollList$j.dat" "NcollList$k.dat"
+                mv "NpartList$j.dat" "NpartList$k.dat"
+                mv "usedParameters$j.dat" "usedParameters$k.dat"
+                mv "NgluonEstimators$j.dat" "NgluonEstimators$k.dat"
+                ((k--))
+        done
+        cd ..
+done
+echo "Done renaming files 1"
+
+c=126
+for ((i=2; i<3; i++))
+do
+        folder="run-$i"
+        cd "$folder" || { echo "Failed to cd into $folder"; exit 1; }
+    
         for ((j=0;j<125;j++))
         do
                 echo "moving item $c"
@@ -23,13 +43,10 @@ do
         done
         cd ..
 done
-echo "Done renaming files."
+echo "Done renaming files 2"
 
 mkdir "all_files"
-
 
 for d in run-*/; do
           cp -a "$d"* all_files/
   done
-
-echo "Done copying"
