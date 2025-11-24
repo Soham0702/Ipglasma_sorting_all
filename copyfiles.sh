37edfe5a9bbdd41e85adcd2813995270e0beeb04
Soham Banerjee <84960194+Soham0702@users.noreply.github.com>
2025-11-25 00:53:05 +0530

    Update copyfiles.sh

--- copyfiles.sh
+++ copyfiles.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 dirs=(0-5_50 5-10_50 10-20_100 20-30_100 30-40_100 40-50_100)
 for d in "${dirs[@]}"; do mkdir -p "$d"; done
 
-mapfile -t ids < <(awk '{print $1}' somelist.dat)                                                                                                                                                                                                                                                                                                                                            
+mapfile -t ids < <(awk '{print $1}' chosenlist.dat)                                                                                                                                                                                                                                                                                                                                            
 
 sizes=(50 50 100 100 100 100)
 
